ad62a11123d24225a7cfe93a7b2653ca2544fdd0
Mathias <mathiaswi177@gmail.com>
2020-06-17 22:42:36 -0700

    tabularQ start

---
+++ tabularQ.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 22:40:11 2020
+
+@author: Mathias
+"""
+
+
+class tabularQ_player:
+    
+    state = [[' ',' ',' '],
+              [' ',' ',' '],
+              [' ',' ',' ']]
+    
+    def hashBoard:
+        return
+    
+    def trainingCycle:
+        return
+    
+    
