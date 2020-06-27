f531ab1655949ac365fddfb04434a91d24ea5850
Mathias <mathiaswi177@gmail.com>
2020-06-26 19:46:58 -0700

    Got started on algorithm to repeatedly spam board with moves so it can learn the best positions

--- tabularQ.py
+++ tabularQ.py
@@ -4,6 +4,7 @@ Created on Wed Jun 17 22:40:11 2020
 
 @author: Mathias
 """
+import random
 
 
 class tabularQ_player:
@@ -27,26 +28,66 @@ class tabularQ_player:
         
         i = 0
         while(i < training_cycles):
+            
+            if player == 'X':
+                tiles = ['X', 'O']
+            else:
+                tiles = ['O', 'X']
+                
+            states_list = []
+            moves_list = []
         
             game_state = "In Progress"
             
             while(game_state == "In Progress"):
             
                 hashed_board = self.hashBoard(state)
-                #Add hashed_board to states list
                 
-                #If AI is X, choose random tile, add to moves_list
+                available_moves = self.empty_tiles(state)
+                length = len(available_moves)
+                randMove = random.randint(1, length)
                 
-                #If AI is O, choose random tile, add to moves_list
+                if not self.game_over(state):
+                    self.play_move(state, 'X', randMove)
+                    
+                #If AI is X, add to states_list and moves_list
+                if player == 'X':
+                    states_list.append(hashed_board)
+                    moves_list.append(randMove)
+                
+                winner = self.check_winner(state, 'X')    
+           
+                if winner is not None:
+                    print(str(winner) + "won")
+                    game_state = "Done"
+                
+                #Recalculate available_moves and randMove
+                available_moves = self.empty_tiles(state)
+                length = len(available_moves)
+                randMove = random.randint(1, length)
+                
+                if not self.game_over(state):
+                    self.play_move(state, 'O', randMove)
+                    
+                #If AI is O, add to states_list and moves_list
+                if player == 'O':
+                    states_list.append(hashed_board)
+                    moves_list.append(randMove)
+                    
+                winner = self.check_winner(state, 'O')
+                if winner is not None:
+                    print(str(winner) + "won")
+                    game_state = "Done"
                 
                 #Calculate quality starting with last state and move
                 
-            i+=1;
+            i+=1
+            #Resetting board happens out here
                 
         
         return #Not sure this function even returns, just fills in hash map
     
-    def check_winner(self, player):
+    def check_winner(self, state, player):
         '''
         Checks whether a player 'X' or 'O' has won by going through self.game_state
         and seeing if there's a three in a row of 'X' or 'O' markers.
@@ -66,14 +107,14 @@ class tabularQ_player:
         
         #Check combinations
         win_states = [
-        [ self.game_state[0][0], self.game_state[0][1], self.game_state[0][2] ],
-        [ self.game_state[1][0], self.game_state[1][1], self.game_state[1][2] ],
-        [ self.game_state[2][0], self.game_state[2][1], self.game_state[2][2] ],
-        [ self.game_state[0][0], self.game_state[1][0], self.game_state[2][0] ],
-        [ self.game_state[0][1], self.game_state[1][1], self.game_state[2][1] ],
-        [ self.game_state[0][2], self.game_state[1][2], self.game_state[2][2] ],
-        [ self.game_state[0][0], self.game_state[1][1], self.game_state[2][2] ],
-        [ self.game_state[2][0], self.game_state[1][1], self.game_state[0][2] ],
+        [ state[0][0], state[0][1], state[0][2] ],
+        [ state[1][0], state[1][1], state[1][2] ],
+        [ state[2][0], state[2][1], state[2][2] ],
+        [ state[0][0], state[1][0], state[2][0] ],
+        [ state[0][1], state[1][1], state[2][1] ],
+        [ state[0][2], state[1][2], state[2][2] ],
+        [ state[0][0], state[1][1], state[2][2] ],
+        [ state[2][0], state[1][1], state[0][2] ],
         ]
         
         if [player, player, player] in win_states:
@@ -89,6 +130,23 @@ class tabularQ_player:
             return None, "Draw" #If draw_flag still 0 then out of available tiles
         
         return None
+    
+    def game_over(self, state):
+        '''
+        Checks if the game is over.
+
+        Parameters
+        ----------
+        state : 
+            The current board state. 
+
+        Returns
+        -------
+        bool
+            True or false if a win is detected for either player designated by 'X' or 'O'.
+
+        '''
+        return self.wins(state, 'X') or self.wins(state, 'O')
     
     def play_move(self, state, player, move_num):
         '''
@@ -111,17 +169,16 @@ class tabularQ_player:
             self.game_state[int((move_num-1)/3)][(move_num-1)%3] = player
         else:
             move_num = int(input("Position is not empty. Choose again: "))
-            Board.play_move(self, state, player, move_num)
+            self.play_move(self, state, player, move_num)
             
-    def empty_tiles(self):
+    def empty_tiles(state):
         '''
         Find available tiles in self.state.
         '''
         tiles = []
         for i in range(3):
             for j in range(3):
-                #if self.state[i][j] is ' ':
-                if self.state[i][j] == ' ':
+                if state[i][j] == ' ':
                     tiles.append(i*3+(j+1))
         return tiles
     
@@ -144,5 +201,16 @@ class tabularQ_player:
             return True
         else:
             return False
+        
+    def print_board(state):
+            '''
+            Prints the board
+            '''
+            print(str(state[0][0]) + ' | ' + str(state[0][1]) + ' | ' + str(state[0][2]))
+            print('---------')
+            print(str(state[1][0]) + ' | ' + str(state[1][1]) + ' | ' + str(state[1][2]))
+            print('---------')
+            print(str(state[2][0]) + ' | ' + str(state[2][1]) + ' | ' + str(state[2][2]))
+            print()
     
     
